4f1f5a5dfada75d8031ff663d3737202967bb34f
majjigiprajwal <prajwal1majjigi@gmail.com>
2023-08-27 15:21:00 +0530

    filtering routes

--- routes/admin.js
+++ routes/admin.js
@@ -3,14 +3,14 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/add-products",(req,res,next)=>{
-    res.send(`<form action="/product" method="POST"><input type="text" name="title">
+    res.send(`<form action="/admin/product" method="POST"><input type="text" name="title">
                <input type="text" name="size" >
                <button type="submit">Add</button> </form>`)
 })
 
 router.post('/product',(req,res)=>{
    console.log(req.body);
-   res.redirect("/")
+   res.redirect("/shop")
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 
 app.use(bodyParser.urlencoded())
 
-app.use(adminRoute);
-app.use(shopRoute)
+app.use("/admin",adminRoute);
+app.use("/shop",shopRoute)
 
 app.use("/",(req,res,next)=>{
     res.status(404).send(`<h1>Page Not Found</h1>`)
